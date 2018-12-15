666e97d87175e71411da3467d0829a2aba9e3bc3
trueshun <zchavez@csumb.edu>
2018-12-15 06:24:35 -0800

    Finished first quest item

--- mainProject.py
+++ mainProject.py
@@ -1,6 +1,6 @@
 #name: Zobeyda Chavez
 #date: 11-28-18
-#description: text-based adventure game in which you play a character who finds a diary detailing a legendary sandwhich
+#description: text-based adventure game in which you play a character who finds a diary detailing a legendary sandwhich.
 #			that he made from special ingrediants surrounding the land. Do you make the sandwhich? Or do you fall
 #			victim to treachery on your way? Your worst enemy might just be yourself.
 
@@ -72,7 +72,7 @@ def checkFridge():
 	if choice2 in answer_a:
 		checkCabinet()
 	else:
-		say("""You grab the bottle of ketchup, and for a second consider grabbing a cup, before opening the 
+		say("""You grab the bottle of ketchup - and for a second think you shouldn't - before opening the 
 			bottle and chugging a mix of semi-sweet/tart tomato puree.\n
 			God, is this your lowest moment yet?
 			""")
@@ -95,9 +95,9 @@ def checkCabinet():
 		As you open the diary, your eyes glaze over the words, excitement fading. \n
 
 		The diary details the information on a 'Legendary' sandwhich, with a hand-drawn map on the front page of the diary. You notice that
-		the map looks like the aread surrounding the cabin. The author - your grandpa? - mentions how excuisite the sandwhich is, 
+		the map looks like the aread surrounding the cabin.\n The author - your grandpa? - mentions how excuisite the sandwhich is, 
 		the taste unparallel to all sandwhich's he has eaten. He spends a page talking about how he can die now that he's eaten this 
-		'legendary' sandwhich. There's somthing about a warning on the last few pages, but what looks like mustard stains the pages.
+		'legendary' sandwhich.\n There's somthing about a warning on the last few pages, but what looks like mustard stains the pages.
 		You can only make out the words: "shine bright".\n
 
 		This has got to be a joke, right?\n
@@ -172,19 +172,61 @@ def getItem():
 		 	jerky = True
 		 	lettuce()
 		else:#this option will GIVE flashlight and NO jerky - AB
-		 	print("You shove it deeper in the drawer, disgust on your face.\n") 
+		 	print("You shove the slim jim deeper in the drawer, disgust on your face.\n") 
 		 	jerky = False
 		 	lettuce()
 	elif choice4 in answer_b:# NO flashlight and NO jerky - B
 		print("Yeah, not bothering looking for that.\n")
 		flashlight = False
-		jerky
+		jerky = False
 		lettuce()
 
-#first item on quest lettuce
+#first item on quest - lettuce!
 def lettuce():
-	print("this si lettuce")
+	global flashlight
+	say("""
+		It takes about 20 minutes, but you come to the field detailed in the diary.\n
+		... At least you think you're in the right spot.\n
+		You walk around, tripping over the rocks. Nothing dangerous so far.\n
+		""")
+	if flashlight == True:
+		say("""
+			You turn on the flashlight, pointing it towards the field before you.\n
+			You notice something strange as you stare into the earthy-smelling field.\n
 
+			There's an empty patch on the field, with numerous mushrooms in a circle around it. Without your flashlight you'd never have seen it.
+			Slowly you approach the circle, stopping just outside the circle of mushrooms. There in the center is a head of the must 
+			luscious lettuce you've ever seen. You take a cautionary step forward, before stopping.\n
+
+			These mushrooms... you remember your grandpa telling you to be carful about stepping into a ring of mushrooms. What were his exact words?
+			'Don't do it!'\n
+
+			Cautiously you lean forward, quickly snatching the head of lettuce with ease. As you do so, the ground within the mushroom circle gives
+			way, the dirt falling into a dark abyss.\n
+
+			'Fairy circle' you mutter, the memory of learning them rushing back you you as you hold the lettuce tight.
+			You shine the light down the hole, seeing no end in sight.\n
+
+			You place the head of lettuce into your bag, the validity of the diary no longer doubted as you head to the next destination.
+			""")
+		meat()
+	else:
+		say("""
+			You keep falling on the moist field, your shoes coated heavily in mud. You're not sure if this is the right place,
+			hell, you're barely sure you know the way back home.\n
+			This was a horrible idea. Why did you think that leaving home without a flashlight was a good idea?\n 
+			Dumbass, that's you for sure.\n
+			Great joke, Grandad.\n
+			As you stand up once again from the dirt, you reach a muddy hand into your bag, tossing the diary into the field.
+			You walk glumly back in what you think is the direction of home. Hungry and dejected, you're reminded of why you hate life.
+			""")
+		replay()
+
+#second ingrediant in quest - meat
+def meat():
+	print("meat")
+	replay()
+	
 #give option to replay game
 def replay():
 	global playAgain
